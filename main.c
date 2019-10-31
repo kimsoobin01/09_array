12bf1a4747dca36c5c8a380ac368e19cf2e22c3f
kimsoobin01 <55084058+kimsoobin01@users.noreply.github.com>
2019-10-31 15:36:19 +0900

    #3

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#define SIZE 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
