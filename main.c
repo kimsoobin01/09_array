ae7049d688c9243d73ccb59bf9ce9149d920fac8
kimsoobin01 <55084058+kimsoobin01@users.noreply.github.com>
2019-10-31 15:29:30 +0900

    #3

--- main.c
+++ main.c
@@ -3,18 +3,20 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
-	int data[5];
+int main(void)
+{
 	int i;
-	 
-	data[0]=10;
-	data[1]=20;
-	data[2]=30;
-	data[3]=50;
-	data[4]=40;
-	for(i=0;i<5;i++)
-		printf("data[%d]is %d\n",i, data[i]);
+	int grade[SIZE];
+	int score[SIZE];
+	
+	for(i=0;i<SIZE;i++)
+	 grade[i]=rand()%100;
+	
+	for(i=0;i<SIZE;i++)
+		score[i]=grade[i]; 
 		
+	for(i=0;i<SIZE;i++)
+		printf("score[%d]=%d\n",i,score[i]);
 		
-	return 0;
+	return 0;	 
 }
